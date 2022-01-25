2a750befa1d63ee86a09d4d282349afc46c08d50
mohsen bagheri <bagheriimohsen@gmail.com>
2022-01-25 13:43:44 +0330

    add BaseService and BaseRepository

---
+++ app/Services/BaseService.php
@@ -0,0 +1,71 @@
+<?php
+
+
+namespace App\Services;
+
+abstract class BaseService
+{
+    abstract public function repository(): string;
+
+    public $repository;
+
+    public function __construct()
+    {
+        $this->makeRepository();
+    }
+
+    public function makeRepository()
+    {
+        $this->repository = app()->make($this->repository());
+    }
+
+    public function create(array $data)
+    {
+        return $this->repository->create($data);
+    }
+
+    public function update(string $id, array $data): int
+    {
+        return $this->repository->update($id, $data);
+    }
+
+    public function updateWithColumn(string $column, string $value, array $data): int
+    {
+        return $this->repository->updateWithColumn($column, $value, $data);
+    }
+
+    public function find(string $column, string $value)
+    {
+        return $this->repository->find($column, $value);
+    }
+
+    public function firstOrFail(string $column, string $value)
+    {
+        return $this->repository->firstOrFail($column, $value);
+    }
+
+    public function delete(string $column, string $value)
+    {
+        return $this->repository->delete($column, $value);
+    }
+
+    public function get($with = [])
+    {
+        return $this->repository->get($with);
+    }
+
+    public function getWithPaginate(null|array $queries = null, array $with = [], $sort = "created_at", $direction = "desc", $perPage = 15): \Illuminate\Contracts\Pagination\LengthAwarePaginator
+    {
+        return $this->repository->getWithPaginate($queries, $with, $sort, $direction, $perPage);
+    }
+
+    public function firstOrCreate($queries, $data)
+    {
+        return $this->repository->firstOrCreate($queries, $data);
+    }
+
+    public function search($value, $sort = 'created_at', $direction = 'desc', $perPage = 15)
+    {
+        return $this->repository->search($value, $sort, $direction, $perPage);
+    }
+}
