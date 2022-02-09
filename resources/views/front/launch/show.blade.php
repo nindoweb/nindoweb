676abe806da7d8ba382084830f46f1464d10e182
mohsen bagheri <bagheriimohsen@gmail.com>
2022-02-09 11:29:52 +0330

    modify and change launch page design

--- resources/views/front/launch/show.blade.php
+++ resources/views/front/launch/show.blade.php
@@ -3,61 +3,59 @@
 @section('title', __('Title'))
 
 @section('content')
-    <section id="section1" class="d-flex flex-column px-5">
-        <h1 class="maniTitle d-flex">
-            <div class="colorTitle">{{__('You are about to')}} <span style="color: red;">{{__('LAUNCH')}}</span></div>
-        </h1>
-        <h3>
-            {{ __('means you are in good hands!') }}
-        </h3>
-        <p class="mt-2">
-            {{ __('Design, develop, test, launch just in a snap.') }}
-        </p>
-    </section>
 
-    <div class="col-sm-8">
-        @include('messages.message')
-        @include('messages.errors')
-    </div>
+    <main>
 
-    <section id="LunchSection-1" class="px-5">
+        <div class="container-custom d-flex flex-column justify-content-between">
+            <section id="teamHeader" class="d-flex flex-column">
+                <h1>
+                    {{__('Nindo')}} <span style="color: red;margin-left: -5px;">L</span>AUNCH
+                </h1>
 
-        <div class="d-flex justify-content-between flex-column">
-            <form action="{{ route('launch.store') }}" method="post">
-                @csrf
-                <div class="d-flex justify-content-between flex-column flex-lg-row" >
-                    <div>
-                        <fieldset class="d-flex mr-5" style="position: relative;">
-                            <legend>
-                                {{ __('E-Mail') }}
-                            </legend>
-                            <input  type="email" name="email">
+                <p class="mt-3">
+                    {{__('Get to know the pros you work with.')}}
+                </p>
+            </section>
+            <div class="container">
+                @include('messages.message')
+                @include('messages.errors')
+            </div>
+            <section id="LunchSection-1">
 
-                        </fieldset>
-                    </div>
 
-                    <div>
-                        <fieldset class="d-flex " style="position: relative;">
-                            <legend>
-                                {{ __('Your name (or company)') }}
-                            </legend>
-                            <input  type="text" name="company">
-                        </fieldset>
-                    </div>
+
+                <div class="parentLunch d-flex justify-content-between flex-column ">
+                    <form action="{{ route('launch.store') }}" method="post">
+                        @csrf
+                        <div class="d-flex justify-content-between flex-column flex-lg-row">
+                            <fieldset class="d-flex  mr-5" style="position: relative;">
+                                <legend>
+                                    {{__('E-Mail')}}
+                                </legend>
+                                <input  type="email" name="email" value="{{ old('email') }}" id="">
+                            </fieldset>
+                            <fieldset class="d-flex mt-5 mt-lg-0 justify-content-end" style="position: relative;">
+                                <legend id='userName'>
+                                    {{__('Your name (or company)')}}
+                                </legend>
+                                <input  type="text" name="company" value="{{ old('company') }}" id="">
+                            </fieldset>
+                        </div>
+                        <input name="honeypot" value="{{null}}" type="hidden">
+                        <div class="textAria d-flex justify-content-end">
+                            <p>
+                                {{__('Your name (or company)')}}
+                            </p>
+                            <textarea name="description">{{ old('description') }}</textarea>
+                        </div>
+                        <div class="d-flex justify-content-end">
+                            <button class="d-block py-2" type="submit">{{__('Submit')}}</button>
+                        </div>
+                    </form>
                 </div>
 
-                <div class="textAria d-flex justify-content-end">
-                    <p>
-                        {{ __('Description') }}
-                    </p>
-                    <textarea name="description"></textarea>
-                </div>
-                <input type="hidden" name="honeypot" value="{{null}}">
-                <div class="d-flex justify-content-end">
-                    <button class="d-block py-2" type="submit">{{ __('Submit') }}</button>
-                </div>
-            </form>
+            </section>
         </div>
 
-    </section>
+    </main>
 @endsection
--- resources/views/messages/message.blade.php
+++ resources/views/messages/message.blade.php
@@ -1,32 +1,42 @@
 @if(session('success') != '')
-    <div class="alert alert-success alert-dismissible fade show col-sm-12" role="alert">
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>{{__('Success')}}</strong>
+        <hr />
         {{ session('success') }}
     </div>
 @endif
 
 @if(session('error') != '')
-    <div class="alert alert-danger alert-dismissible fade show col-sm-12" role="alert">
+    <div class="alert alert-error alert-dismissible fade show" role="alert">
+        <strong>{{__('Error')}}</strong>
+        <hr />
         {{ session('error') }}
     </div>
 @endif
 
 
 @if(session('warning') != '')
-    <div class="alert alert-warning alert-dismissible fade show col-sm-12" role="alert">
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>{{__('Warning')}}</strong>
+        <hr />
         {{ session('warning') }}
     </div>
 @endif
 
 
 @if(session('info') != '')
-    <div class="alert alert-info alert-dismissible fade show col-sm-12" role="alert">
+    <div class="alert alert-info alert-dismissible fade show" role="alert">
+        <strong>{{__('Info')}}</strong>
+        <hr />
         {{ session('info') }}
     </div>
 @endif
 
 
 @if(session('primary') != '')
-    <div class="alert alert-primary alert-dismissible fade show col-sm-12" role="alert">
+    <div class="alert alert-primary alert-dismissible fade show" role="alert">
+        <strong>{{__('')}}</strong>
+        <hr />
         {{ session('primary') }}
     </div>
 @endif
